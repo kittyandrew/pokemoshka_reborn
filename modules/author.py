0a4d7e3a0bcbb8bd2358d5742773a89b53be7dfa
andrebcd4 <andrebc375@gmail.com>
2020-02-06 16:39:56 +0200

    fix

--- modules/author.py
+++ modules/author.py
@@ -13,13 +13,14 @@ MSG = \
 `fiverr:` [buy bot](https://www.fiverr.com/kitty_andrew?up_rollout=true)"""
 
 async def init(bot):
-    @bot.on(events.NewMessage(pattern=r"^(/author|/author@pokemonchik_bot)$"))
+    @bot.on(events.NewMessage(pattern=r"^/author($|/@pokemonchik_bot$)"))
     @error_logger
     async def author_handler(event):
         if chats.get(event.chat_id, None):
             delta = chats[event.chat_id] - time.time()
-            if delta > 10:
+            if delta > 60:
                 await event.respond(MSG, link_preview=False)
+                chats[event.chat_id] = time.time()
         else:
             chats[event.chat_id] = time.time()
             await event.respond(MSG, link_preview=False)
